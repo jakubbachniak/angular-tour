cc67c5aaa943a3a49637d481c6f718b438b96db6
Jakub Bachniak <jakub_bachniak@mentor.com>
2020-01-16 17:02:56 +0000

    Working in Master/Detail Components
    https://angular.io/tutorial/toh-pt3

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,8 +11,10 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
+  valueofInput: string;
   heroes = HEROES;
   selectedHero: Hero;
+
 
   constructor() { }
 
@@ -22,6 +24,20 @@ export class HeroesComponent implements OnInit {
   // handle li element click event
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  // handle click event of the button in the heroes page.
+  // on click pass the value of the input box to the component
+  // and place it in the class' valueOfInput field
+
+  callPhone(phoneNumber: string) {
+    this.valueofInput = phoneNumber;
+  }
+
+  // after receiving the value from the button click
+  // this function is called from the template to retrieve the value of the input.
+  getPhoneNumber() {
+    return ( this.valueofInput == null ? 'no number provided' : this.valueofInput );
   }
 
 }
